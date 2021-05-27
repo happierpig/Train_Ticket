d5882e3032209ae69b581402e42125b9dadb2372
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-05-27 19:52:21 +0800

    update

--- Engine.h
+++ Engine.h
@@ -44,11 +44,11 @@ private:
     unordered_map<string,int> log_in_user ;
     stringstream command_stream ;
     BPlusTree<IndexKey,user> user_tree ;
-    BPlusTree<IndexKey,int,300,300,4373> train_tree ; // todo 存 int -> 节点 pos
+    BPlusTree<IndexKey,int,200,300,5743> train_tree ; // todo 存 int -> 节点 pos
     BPlusTree<IndexKey,ticket_deal> user_deal_tree ;
-    BPlusTree<IndexKey,station,300,300,4373> location_train_tree ; // todo 存 location -> trainID + location_i 类精简数据
-    BPlusTree<pair<IndexKey,date>,ticket_deal> waiting_tree ; // todo  <trainID,date> -> ticket_deal
-    BPlusTree<pair<IndexKey,date>,day_train> day_train_tree ;
+    BPlusTree<IndexKey,station> location_train_tree ; // todo 存 location -> trainID + location_i 类精简数据
+    BPlusTree<pair<IndexKey,date>,ticket_deal,200,100> waiting_tree ; // todo  <trainID,date> -> ticket_deal
+    BPlusTree<pair<IndexKey,date>,day_train,200,100> day_train_tree ;
 
     // todo day_train_tree <trainID,date> -> day_train
 
